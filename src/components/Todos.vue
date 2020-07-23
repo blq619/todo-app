49cdda3a3186a78e695c129d64f5a967a810d7b9
kuknam <blq619@naver.com>
2020-07-23 19:42:03 +0900

    todo publishing

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <div v-for="todo in todos" v-bind:key="todo.id">
+          <TodoItem v-bind:todo="todo"/>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './Todoitem'
+export default {
+    components: {
+        TodoItem
+    },
+    props: {
+        todos: Array,
+    },
+};
+</script>
+
+<style>
+
+</style>
